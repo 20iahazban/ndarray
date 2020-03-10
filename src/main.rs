aefa67b474a550b3c1ed3407430d0cdc6cfce9e2
20iahazban <husziahazban@gmail.com>
2020-03-10 22:32:31 +0100

    use size

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut new_data = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
     let nd = ArrayViewMut::from_shape((2, 2).strides((1, 2)), &mut new_data[..=size]).unwrap();
     let nd = nd.as_standard_layout();
-    let nd_falt = nd.into_shape(4).unwrap();
+    let nd_falt = nd.into_shape(size).unwrap();
     let mut nd_vec = nd_falt.to_owned().into_raw_vec();
     nd_vec.extend(&new_data[size..]);
     dbg!(nd_vec);
